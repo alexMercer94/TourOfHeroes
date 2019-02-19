04b084399b4f724861cad5cde2f7a00ba924d6a8
Anzen Digital <alex.c.valera94@hotmail.com>
2019-02-19 13:56:34 -0600

    Master / Detail

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Destroyer'},
+    {id: 15, name: 'Celeritas'},
+    {id: 16, name: 'Magneta'},
+    {id: 17, name: 'RubberMan'},
+    {id: 18, name: 'Dynama'},
+    {id: 19, name: 'Dr. Strange'},
+    {id: 20, name: 'Batman'},
+    {id: 21, name: 'Superman'},
+    {id: 22, name: 'Hulk'}
+];
